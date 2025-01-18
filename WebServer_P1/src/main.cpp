2353b8cf904e9c58486fa23b5eb843d00199bf5d
CherryTreeGee811 <79172404+CherryTreeGee811@users.noreply.github.com>
2025-01-18 01:09:24 -0500

    Lighten the project and dockerize - JT
    For dockerfile I used the stage build technique along with alpine linux to keep docker image really small.

--- WebServer_P1/src/main.cpp
+++ WebServer_P1/src/main.cpp
@@ -1,12 +1,14 @@
 #include "crow_all.h"
 #include "../include/routes/routes.h"
 
-int main() {
+int main(void) {
     crow::SimpleApp app;
 
     // Register routes
     setupRoutes(app);
 
     // Start the server
-    app.bindaddr("127.0.0.1").port(8080).multithreaded().run();
+    app.bindaddr("0.0.0.0").port(8080).multithreaded().run();
+
+    return 0;
 }
--- WebServer_P1/src/routes/routes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "crow_all.h"
-
-// Function to set up example routes
-void setupRoutes(crow::SimpleApp& app);
